c8445d049ed375e594412bd13126583130b8c2c8
juanp-rojash <juanp.rojash@upb.edu.co>
2024-08-23 00:25:18 -0500

    Estructura Parcial

---
+++ app/src/main/java/parcial1g2/App.java
@@ -0,0 +1,27 @@
+
+package parcial1g2;
+
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        // Logica de solución
+
+    }
+
+    /*  Implemente una funcion llamada calcularTotalImpuestos que cumpla con las siguientes caracteristicas
+        * Recibe: 
+            - Una cadena de caracteres que representa la decada (70, 80, 90, 00, 10, 20, Otra)
+            - Un entero que representa avaluo del vehiculo
+        * Retorna:
+            - Un entero representando el valor total a pagar de los impuestos
+        * Controlar errores retornando -1
+    */
+    //------------------------------------------------------------------
+    
+
+
+    //------------------------------------------------------------------
+
+}
